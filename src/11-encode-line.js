75591e4c0e856e7b9da9f87f0a4c1a9f623f1d2e
Marina Borisyuk <marinaborisyuk1@gmail.com>
2021-04-11 21:04:23 +0300

    feat: add task #11

--- src/11-encode-line.js
+++ src/11-encode-line.js
@@ -8,8 +8,22 @@
  * For aabbbc should return 2a3bc
  *
  */
-function encodeLine(/* str */) {
-  throw new Error('Not implemented');
+function encodeLine(str) {
+  // throw new Error('Not implemented');
+  const arr = str.split('');
+  let count = 0;
+  for (let i = 1; i < arr.length; i++) {
+    if (arr[i] === arr[i - 1]) {
+      count++;
+      if (arr[i] !== arr[i + 1]) {
+        const index = arr.indexOf(arr[i]);
+        arr.splice(i - count, count, count + 1);
+        count = 0;
+        i = index - 1;
+      }
+    }
+  }
+  return arr.join('');
 }
 
 module.exports = encodeLine;
